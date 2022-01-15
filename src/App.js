0e4f0fb46a0f0282edebe62aa7319e29ff0ccd11
Renan <renan.paiva@al.infnet.edu.br>
2022-01-15 18:11:22 -0300

    Correções - corta header

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import Logo from './assets/img/Logo-saint-manager.png'
 
 function App() {
   return (
-    <Navbar expand="md" className= 'main-navbar'>
-      <Container>
-        <Navbar.Brand href="#home">
-         <Button variant="link" href="#link" className='main-navbar border-0'  > <img src={Logo} alt= "Logo Saint Manager" width={46.58} height={41}/>
-         </Button>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#link" className='btn-remove'>Home</Nav.Link>
-            <Nav.Link href="#link" className='btn-remove'>Eventos</Nav.Link>
-            <Nav.Link href="#link">Entrar</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar expand="md" className='main-navbar'>
+        <Container>
+          <Navbar.Brand href="/">
+            <Button variant="link" href="/" className='main-navbar border-0'  > <img src={Logo} alt="Logo Saint Manager" width={46.58} height={46.58} />
+            </Button>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/" className='btn-remove'>Home</Nav.Link>
+              <Nav.Link href="/eventos" className='btn-remove'>Eventos</Nav.Link>
+              <Nav.Link href="/login">Entrar</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
