f36b238bff320df2ba46a54e8d9d1279e1e4aad2
Renan <renan.paiva@al.infnet.edu.br>
2022-01-14 22:02:47 -0300

    Altera README

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Button } from 'react-bootstrap';
 function App() {
   return (
     <div>
+      <h1>Saint Manager</h1>
+      
      <Button variant="primary">Primary</Button>
     </div>
   );
