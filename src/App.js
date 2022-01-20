baa3eb0aafbbaa6034818cadefdf46e2584f69c9
Renan <renan.paiva@al.infnet.edu.br>
2022-01-20 14:19:49 -0300

    Banner Home responsivo

--- src/App.js
+++ src/App.js
@@ -30,15 +30,15 @@ function App() {
       <main>
         <Container>
           <Container>
-            <Row className='shadow-sm banner'>
-              <Col lg={4} >
-                <img src={Banner} alt="Banner de Igreja" className='banner-img' />
+            <Row className='shadow banner rounded'>
+              <Col lg={4} className='rounded-left banner-img-col' >
+                <img src={Banner} alt="Banner de Igreja" className='banner-img rounded-left' />
               </Col>
-              <Col lg={8} className="p-10">
+              <Col lg={8} className="p-10 banner-text">
                 <h1 className='text-sm-start'>Venha  conhecer o Saint Manager!</h1>
-                <p className='ps-5' >Membros podem se  inscrever a eventos,  visitas e saber muito mais!</p>
-                <p className='text-sm-center'>Fique sempre atento  as datas dos eventos, para não perder!</p>
-                <p className='text-sm-end'>Que Deus  abençoe a todos!  Volte Sempre!</p>
+                <p className='ps-3' >Membros podem se  inscrever a eventos,  visitas e saber muito mais!</p>
+                <p className='text-center'>Fique sempre atento  as datas dos eventos, para não perder!</p>
+                <p className='text-end'>Que Deus  abençoe a todos!  Volte Sempre!</p>
               </Col>
             </Row>
           </Container>
