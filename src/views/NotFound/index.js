4ecba83515d34f4b2dbbbd4f7952f0f5df7f61e1
Renan <renan.paiva@al.infnet.edu.br>
2022-01-20 18:33:33 -0300

    Corta página NotFound

--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -1,9 +1,14 @@
+import { Container } from "react-bootstrap";
 import { Layout } from "../../components/Layout";
 
-export function NotFoundView (){
-    return(
-      <Layout>   
-      <h1>Página não encontrada!</h1>      
-      </ Layout>
-    )
-  }
+export function NotFoundView() {
+  return (
+    <Layout>
+      <Container className="text-center mt-4">
+        <h1>Página não encontrada!</h1>
+        <p>A página que você está procurando não foi encontrada ou foi movida.</p>
+        <p>Utilize o Menu principal ou retorne para pagina inicial para navegar entre as páginas.</p>
+      </Container>
+    </ Layout>
+  )
+}
