933105accae769562b27ada7910c84be0511d7fe
Renan <renan.paiva@al.infnet.edu.br>
2022-01-22 20:43:05 -0300

    Separando os Componentes do Evento inscrição

--- src/views/EventoDetail/index.js
+++ src/views/EventoDetail/index.js
@@ -1,6 +1,7 @@
 import { Layout } from "../../components/Layout";
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { CardEventoDetail } from "./cardEventoDetail";
+import { Inscritos } from "./inscritos";
 
 
 
@@ -8,8 +9,8 @@ export function EventoDetailView() {
     return (
         <Layout>
             <Container>
-                <CardEventoDetail />           
-                <h2>Formulário de Inscrição</h2>            
+                <CardEventoDetail />
+                <Inscritos />
             </Container>
         </Layout>
     )
---
+++ src/views/EventoDetail/inscritos.js
@@ -0,0 +1,20 @@
+import { Table } from "react-bootstrap";
+
+export function Inscritos() {
+    return (
+        <>
+            <h2>Formulário de Inscrição</h2>
+
+            <h2>Quem Vai?</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+            </Table>
+
+        </>
+    )
+}
