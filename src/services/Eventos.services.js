d1487e643ca6e39e915823026003f69b2b2a556c
Renan <renan.paiva@al.infnet.edu.br>
2022-02-09 19:00:04 -0300

    Implementando tipo .env

--- src/services/Eventos.services.js
+++ src/services/Eventos.services.js
@@ -1,4 +1,5 @@
 export const getEventoById = async (id) => {
+    console.log(process.env.REACT_APP_API_URL)
     const response = await fetch(`http://localhost:3001/eventos/${id}?_embed=inscriptions`)
     if (!response.ok) {
         throw new Error('Response not OK.')
