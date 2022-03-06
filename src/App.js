2b5ea1a517311d0b5fb97d6e954d0b6c0d64db15
Renan <renan.paiva@al.infnet.edu.br>
2022-03-06 20:15:45 -0300

    Protege rotas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute";
 import { AdminAddEventoView } from "./views/AdminAddEvento";
 import { AdminEditEventoView } from "./views/AdminEditEvento";
 import { AdminEventosView } from "./views/AdminEventos";
@@ -15,10 +16,10 @@ function App() {
       <Route path="/" element={<HomeView />} />
       <Route path="/eventos" element={<EventoView />} />
       <Route path="/eventos/:id" element={<EventoDetailView />} />
-      <Route path="/portal" element={<DashboardView />} />
-      <Route path="/portal/eventos" element={<AdminEventosView />} />
-      <Route path="/portal/eventos/add" element={<AdminAddEventoView />} />
-      <Route path="/portal/eventos/:id" element={<AdminEditEventoView />} />
+      <Route path="/portal" element={<PrivateRoute> <DashboardView /> </PrivateRoute>} />
+      <Route path="/portal/eventos" element={<PrivateRoute userTypes={[1]}> <AdminEventosView /> </PrivateRoute>} />
+      <Route path="/portal/eventos/add" element={<PrivateRoute userTypes={[1]}> <AdminAddEventoView /> </PrivateRoute>} />
+      <Route path="/portal/eventos/:id" element={<PrivateRoute userTypes={[1]}> <AdminEditEventoView /> </PrivateRoute>} />
       <Route path="/portal/login" element={<LoginView />} />
       <Route path="*" element={<NotFoundView />} />
     </Routes>
